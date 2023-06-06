ad67753a1095820e00c51b5e5883f01545dc24e5
Paiman Rasoli <paimanrasoli789@gmail.com>
2023-06-06 18:40:43 +0430

    create the migrations

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -2,10 +2,26 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Migrations {
+
  address public owner;
  uint public last_completed_migration;
 
  constructor () public {
     owner = msg.sender;
  }
+
+ modifier onlyOwner {
+  require(msg.sender == owner);
+  _;
+ }
+
+ function set_completed(uint completed) public onlyOwner {
+  last_completed_migration = completed;
+ }
+
+ function upgrade(address new_address) public onlyOwner {
+  Migrations upgrade = Migrations(new_address);
+  upgrade.set_completed(last_completed_migration);
+ }
+
 }
